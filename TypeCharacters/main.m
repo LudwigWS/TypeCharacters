4ce8a9e9a66c60e3d29c50977ee713fa00e1bc98
LudwigWS <LudwigWS@outlook.com>
2024-05-09 11:09:18 +0800

    support stdin

--- TypeCharacters/main.m
+++ TypeCharacters/main.m
@@ -7,6 +7,8 @@
 //  LINK: https://apple.stackexchange.com/questions/288536/is-it-possible-to-keystroke-special-characters-in-applescript/289046#289046
 
 #import <Foundation/Foundation.h>
+#import <CoreGraphics/CoreGraphics.h>
+#import <Carbon/Carbon.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
@@ -29,6 +31,43 @@ int main(int argc, const char * argv[]) {
                 [NSThread sleepForTimeInterval:0.004]; // wait 4/1000 of second, 0.002 it's OK on my computer, I use 0.004 to be safe, increase it If you still have issues
             }
             CFRelease(keyEvent);
+        } else {
+            @autoreleasepool {
+                    char buffer[1024]; // A buffer to store input lines
+                    while (fgets(buffer, sizeof(buffer), stdin)) {
+                        NSString *lineString = [NSString stringWithUTF8String:buffer];
+                        lineString = [lineString stringByTrimmingCharactersInSet:[NSCharacterSet newlineCharacterSet]]; // Trim newlines
+
+                        CGEventSourceRef source = CGEventSourceCreate(kCGEventSourceStateHIDSystemState);
+                        for (int i = 0; i < [lineString length]; i++) {
+                            UniChar c = [lineString characterAtIndex:i];
+                            CGEventRef keyDown = CGEventCreateKeyboardEvent(source, 0, true);
+                            CGEventRef keyUp = CGEventCreateKeyboardEvent(source, 0, false);
+                            CGEventKeyboardSetUnicodeString(keyDown, 1, &c);
+                            CGEventKeyboardSetUnicodeString(keyUp, 1, &c);
+                            CGEventPost(kCGHIDEventTap, keyDown);
+                            CGEventPost(kCGHIDEventTap, keyUp);
+                            CFRelease(keyDown);
+                            CFRelease(keyUp);
+                            [NSThread sleepForTimeInterval:0.004];
+                        }
+                        // After each line, send an enter key event to simulate the end of the line
+                        UniChar enter = '\n';
+                        CGEventRef keyDownEnter = CGEventCreateKeyboardEvent(source, kVK_Return, true);
+                        CGEventRef keyUpEnter = CGEventCreateKeyboardEvent(source, kVK_Return, false);
+                        CGEventKeyboardSetUnicodeString(keyDownEnter, 1, &enter);
+                        CGEventKeyboardSetUnicodeString(keyUpEnter, 1, &enter);
+                        CGEventPost(kCGHIDEventTap, keyDownEnter);
+                        CGEventPost(kCGHIDEventTap, keyUpEnter);
+                        CFRelease(keyDownEnter);
+                        CFRelease(keyUpEnter);
+
+                        [NSThread sleepForTimeInterval:0.004];
+                        CFRelease(source);
+                    }
+                }
+                return 0;
+            
         }
     }
     return 0;
